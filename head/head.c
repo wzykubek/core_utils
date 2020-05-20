fe9663517007ed8b237600396b4a5a614c1637bf
samedamci <samedamci@disroot.org>
2020-05-20 15:25:49 +0200

    Add `-v` flag to head tool
    Also add info comment on top of the file

--- head/head.c
+++ head/head.c
@@ -2,14 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Now this implementation support only one file and
+// arguments must be in the proper sequence.
+
 int main(int argc, char **argv) {
   int output_lines_num = 11;
+  int verbose = 0;
 
   if ((argc == 2) && !strncmp(argv[1], "-h", 3)) {
-    fputs("Usage: head [-h] [-n] file\n", stdout);
+    fputs("Usage: head [OPTION] file\n", stdout);
     exit(EXIT_SUCCESS);
   } else if ((argc >= 2) && !strncmp(argv[1], "-n", 3)) {
     output_lines_num = atoi(argv[2]);
+  } else if ((argc >= 2) && !strncmp(argv[1], "-v", 3))  {
+    verbose = 1;
   }
 
   FILE *fp;
@@ -20,6 +26,9 @@ int main(int argc, char **argv) {
     printf("%s\n", "Specify filename as last argument.");
     exit(EXIT_FAILURE);
   }
+
+  if (verbose == 1)
+    printf("%s%s%s\n", "==> ", filename, " <==");
 
   char ch;
   int lines = 0;
