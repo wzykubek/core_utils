36bb5f69184742e4bb8cd160c740c285f38575cd
samedamci <samedamci@disroot.org>
2020-05-22 21:09:20 +0200

    Add echo command
    Done:
    + echo argument
    + `-n` option (do not output the trailing newline)
    + `-e` option (enable interpretation  of backslash escapes)
    
    Supported escapes:
    \n - new line
    \t - horizontal tab

---
+++ echo/echo.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void show_help() {}
+
+static char *input;
+static int trailing_newline = 1;
+static int escapes_interpretation = 0;
+
+void short_opt(int argc, char **argv, int *j) {
+  for (int i = 1; argv[*j][i]; i++) {
+    switch (argv[*j][i]) {
+    case 'n':
+      trailing_newline = 0;
+      break;
+    case 'e':
+      escapes_interpretation = 1;
+      break;
+    case 'E':
+      // default
+      break;
+    default:
+      show_help();
+      exit(EXIT_SUCCESS);
+    }
+  }
+}
+
+void echo(char *input) {
+  size_t n = strlen(input);
+  for (int i = 0; i < n; i++) {
+    if (escapes_interpretation == 1) {
+      if (input[i] == '\\') {
+        switch (input[++i]) {
+        case 'n':
+          printf("\n");
+          break;
+        case 't':
+          printf("\t");
+          break;
+        }
+      }
+    } else {
+      printf("%c", input[i]);
+    }
+  }
+
+  if (trailing_newline == 1)
+    printf("\n");
+}
+
+int main(int argc, char **argv) {
+  for (int j = 1; argc - j; j++) {
+    if (argv[j][0] == '-') {
+      if (strcmp(argv[j], "--version") == 0) {
+        puts("version 0.1");
+      } else if (strcmp(argv[j], "--help") == 0) {
+        show_help();
+        exit(EXIT_SUCCESS);
+      } else {
+        short_opt(argc, argv, &j);
+      }
+    } else {
+      input = argv[j];
+    }
+  }
+
+  echo(input);
+}
